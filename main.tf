bff26f1141751a40b7785e9fa4ba2bd7067a8031
Cole DeVries <colemandevries@gmail.com>
2023-01-30 20:08:01 -0500

    added latest s3 bucket module updates

--- main.tf
+++ main.tf
@@ -1,13 +1,29 @@
-# update this to demonstrate usage 
-# of the module. If there are more 
-# than one examples that you want 
-# to demonstrate, create additional files in
-# this directory and name them logically. 
+resource "aws_s3_bucket" "this" {
+  bucket        = "${local.stack}-${var.bucket-suffix}"
+  force_destroy = var.force_destroy
+}
 
-module "this" {
-  source = "../"
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
 
-  app         = "example"
-  environment = "dev"
-  program     = "example"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = var.versioning
+  }
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  count = var.enable_logging ? 1 : 0
+
+  bucket        = var.enable_versioning ? aws_s3_bucket.this.id : null
+  target_bucket = var.enable_versioning ? var.log_bucket : null
+  target_prefix = var.enable_versioning ? var.log_prefix : null
 }
--- variables.tf
+++ variables.tf
@@ -10,7 +10,8 @@ variable "env" {
   sensitive   = false
 
   validation {
-    condition = contains(["dev", "qa", "stage", "prod"], var.env)
+    condition     = contains(["dev", "qa", "stage", "prod"], var.env)
+    error_message = "valid values for env are 'dev', 'qa', 'stage', 'prod', and 'nonprod'"
   }
 }
 variable "program" {
@@ -22,4 +23,38 @@ variable "program" {
     condition     = contains(["crdc", "ccdi", "ctos"], var.program)
     error_message = "valid values for program are 'crdc', 'ccdi', and 'ctos'"
   }
+}
+
+variable "bucket_suffix" {
+  type        = string
+  description = "the suffix of the bucket name that follows the name of the stack"
+}
+
+variable "force_destroy" {
+  type        = bool
+  description = "whether to destroy all objects in the bucket prior to deleting bucket"
+  default     = false
+}
+
+variable "versioning" {
+  type        = bool
+  description = "enable object versioning - either 'Enabled' or 'Disabled'"
+  default     = true
+}
+
+variable "log_enabled" {
+  type        = bool
+  description = "enable s3 server-side logging"
+  default     = true
+}
+
+variable "log_bucket" {
+  type        = string
+  description = "the destination log bucket name or id"
+}
+
+variable "log_prefix" {
+  type        = string
+  description = "the prefix for the log directories - if not provided, defaults to root s3 directory"
+  default     = null
 }
