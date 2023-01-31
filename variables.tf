a463fdfa63b74c6a374a48cc39a42ce7c24882f2
Cole DeVries <colemandevries@gmail.com>
2023-01-30 20:28:38 -0500

    updated validation of env

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "env" {
   sensitive   = false
 
   validation {
-    condition     = contains(["dev", "qa", "stage", "prod"], var.env)
+    condition     = contains(["dev", "qa", "stage", "prod", "nonprod"], var.env)
     error_message = "valid values for env are 'dev', 'qa', 'stage', 'prod', and 'nonprod'"
   }
 }
